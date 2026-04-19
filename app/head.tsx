e5e067ef8174901ebff4e5396b011a2e1357a448
our ame <your-email@example.com>
2026-04-19 11:18:22 +0300

    feat: add Google site verification meta tag to head component

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="google-site-verification"
+        content="hfkzuAhdV6jsStuxMJ_xNeOYHidl19gY-qvqk8LEJFA"
+      />
+    </>
+  );
+}
